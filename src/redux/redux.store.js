be2b1c54a2a635bbed484403449ab32fe718fa23
Baraiboapex <31634799+Baraiboapex@users.noreply.github.com>
2020-07-20 16:00:15 -0400

    Really fixed Redux debugger error

--- src/redux/redux.store.js
+++ src/redux/redux.store.js
@@ -2,7 +2,7 @@
 //NOTE: Aways remember to turn off redux debug middleware before deploying to prod!
 //=================================================================================
 
-import {createStore, applyMiddleware, compose } from "redux";
+import {createStore, applyMiddleware /*,compose*/ } from "redux";
 import rootReducer from "./reducers/root.reducer";
 import thunk from "redux-thunk";
 
@@ -11,7 +11,11 @@ const middleWare = [thunk];
 const store = createStore(
     rootReducer,
     {},
-    applyMiddleware(...middleWare)
+    applyMiddleware(...middleWare),
+    //compose(
+       // applyMiddleware(...middleWare),
+       // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+     //)
 );
 
 export default store;
